d31ec6c946ed1a5327a493e4a6e65c24b380e194
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2018-05-01 15:31:54 +0300

    Stop program gracefully.

--- main.go
+++ main.go
@@ -12,41 +12,63 @@ import (
 	"time"
 )
 
-func run(ctx context.Context, run time.Duration, args []string) error {
-	t := time.NewTicker(run)
-	defer t.Stop()
+func run(ctx context.Context, run, grace time.Duration, args []string) error {
+	runT := time.NewTicker(run)
+	defer runT.Stop()
 
-	// start program
-	cmd := exec.CommandContext(ctx, args[0], args[1:]...)
+	// start program in a separate process group to prevent automatic signals propagation
+	cmd := exec.Command(args[0], args[1:]...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Setpgid: true,
+	}
 	if err := cmd.Start(); err != nil {
 		return err
 	}
 
-	// wait for program to exit or for t to tick
+	// receive program exit status asynchronously
 	done := make(chan error, 1)
 	go func() {
 		done <- cmd.Wait()
 	}()
+
+	// wait for ctx to be canceled, program to exit, or for runT to tick
 	select {
+	case <-ctx.Done():
+		// nothing
 	case err := <-done:
 		return err
-	case <-t.C:
+	case <-runT.C:
 		// nothing
 	}
 
-	// signal program to exit
+	// ask program to exit
 	if err := cmd.Process.Signal(syscall.SIGTERM); err != nil {
 		log.Printf("Failed to send SIGTERM: %s", err)
 	}
 
-	// wait for program to exit from SIGTERM or from SIGKILL from the ctx
+	// wait for program to exit, or for graceT to tick; ignore ctx even if it is already canceled
+	graceT := time.NewTicker(grace)
+	defer graceT.Stop()
+	select {
+	case err := <-done:
+		return err
+	case <-graceT.C:
+		// nothing
+	}
+
+	// kill program
+	if err := cmd.Process.Signal(syscall.SIGKILL); err != nil {
+		log.Printf("Failed to send SIGKILL: %s", err)
+	}
+
+	// wait for program to exit
 	return <-done
 }
 
 func main() {
-	runF := flag.Duration("run", time.Minute, "Periad between starting a program and sending it SIGTERM")
+	runF := flag.Duration("run", time.Minute, "Period between starting a program and sending it SIGTERM")
 	graceF := flag.Duration("grace", 10*time.Second, "Period between sending a program SIGTERM and SIGKILL")
 	flag.Usage = func() {
 		fmt.Fprintf(flag.CommandLine.Output(), "Usage: %s [flags] [program] [program arguments]\n", os.Args[0])
@@ -62,7 +84,7 @@ func main() {
 	log.SetPrefix("ruc: ")
 	log.SetFlags(log.Ltime)
 
-	mainCtx, mainCancel := context.WithCancel(context.Background())
+	ctx, cancel := context.WithCancel(context.Background())
 
 	// handle termination signals: first one gracefully, force exit on the second one
 	signals := make(chan os.Signal, 1)
@@ -70,18 +92,15 @@ func main() {
 	go func() {
 		s := <-signals
 		log.Printf("Got %v (%d) signal, shutting down...", s, s.(syscall.Signal))
-		mainCancel()
+		cancel()
 
 		s = <-signals
 		log.Panicf("Got %v (%d) signal, exiting!", s, s.(syscall.Signal))
 	}()
 
 	for {
-		log.Printf("Starting...")
-		ctx, cancel := context.WithTimeout(mainCtx, *runF+*graceF)
-		if err := run(ctx, *runF, flag.Args()); err != nil {
+		if err := run(ctx, *runF, *graceF, flag.Args()); err != nil {
 			log.Fatal(err)
 		}
-		cancel()
 	}
 }
